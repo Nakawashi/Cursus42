b2fae8b338718fee075da3f7437f4c5b4f0b5a20
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-03-01 18:43:26 +0100

    learnmore

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 15:58:50 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/03/01 15:14:19 by lgenevey         ###   ########.fr       */
+/*   Updated: 2022/03/01 18:42:24 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,14 +61,30 @@ void	send_bits(char c)
 
 }
 
-// void	handle_sigusr1(pid_t pid, char *s)
-// {
+//https://github.com/lavrenovamaria/42-minitalk/blob/main/src/client.c
+void	bit_by_bit(int pid, int bit)
+{
+	int	counter;
 
-// }
+	counter = 0;
+	while (counter <= 7)
+	{
+		if ((bit >> counter) & 1)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				ft_putstr_fd("Error signal\n", 1);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				ft_putstr_fd("Error signal\n", 1);
+		}
+		counter++;
+		usleep(100);
+	}
+}
 
-/*
 
-*/
 int	main(int argc, char **argv)
 {
 	if (argc != 3)
@@ -78,14 +94,16 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	pid_t 	pid;
-	char	*msg;
+	char	*message;
 
 	pid = ft_atoi(argv[1]);
-	msg = argv[2];
+	message = argv[2];
 	//struct	sigaction bernard;
 
 	//bernard.sa_handler = &handle_sigusr1; // link our signal handler to signal
 	//sigaction(SIGUSR1, &bernard, NULL);
+
+	// envoyer le message en utilisant
 
 
 
@@ -97,7 +115,7 @@ int	main(int argc, char **argv)
 infos necessaires :
 
 pid
-msg recu
+message recu
 decortiquer la chaine pour transformer les char en bit sur 1 octet chaque
 et envoyer chaque bit 1 à 1 avec kill()
 
