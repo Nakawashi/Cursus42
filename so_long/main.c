5afc4a586eb24ed8063c8e3354f7586c99ce105e
Lucie Genevey <l.genevey@gmail.com>
2022-03-24 01:00:13 +0100

    command to compile in main.c

--- so_long/main.c
+++ so_long/main.c
@@ -1,6 +1,10 @@
-#include <minilibx/mlx.h>
+#include "minilibx/mlx.h"
+#include <unistd.h>
 
 int main()
 {
-
+	write(1, "coucou", 6);
+	return (0);
 }
+
+// gcc -Wall -Wextra -Werror -Iminilibx -Lminilibx -framework OpenGL -framework AppKit main.c
