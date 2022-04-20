a663feea077181b24806abdc92e428d775c48cbc
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-04-20 20:59:19 +0200

    add free 2d table functions

---
+++ so_long/sources/free_2D_table.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_2D_table.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 20:29:16 by lgenevey          #+#    #+#             */
+/*   Updated: 2022/04/20 20:40:46 by lgenevey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static void	ft_free_ptr(void **ptr)
+{
+	if (*ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+	}
+}
+
+void	free_map(char **map)
+{
+	int	i;
+
+	if (!map)
+		return ;
+	i = 0;
+	while (map[i])
+	{
+		ft_free_ptr((void *)&map[i]);
+		i++;
+	}
+	ft_free_ptr((void *)&map);
+}
