3b98c7eece9fe27287cf4df1a45176d9062fd82c
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-05-13 14:38:42 +0200

    try to debug segfault

--- so_long/sources/clean_stuff.c
+++ so_long/sources/clean_stuff.c
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 20:29:16 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/05/09 19:00:23 by lgenevey         ###   ########.fr       */
+/*   Updated: 2022/05/13 14:34:45 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,16 @@ void	free_map(char **map)
 
 int	clean(t_game *game)
 {
-	ft_printf("ESC used to quit\n");
-	// mlx_destroy_image(game->mlx_id, game->img_wall);
-	// mlx_destroy_image(game->mlx_id, game->img_ground);
-	// mlx_destroy_image(game->mlx_id, game->img_player);
-	// mlx_destroy_image(game->mlx_id, game->img_coll);
-	// mlx_destroy_image(game->mlx_id, game->img_exit);
-	//free_map(game->map);
+	//ft_printf("ESC used to quit\n");
+	mlx_destroy_image(game->mlx_id, game->img_ground);
+	mlx_destroy_image(game->mlx_id, game->img_wall);
+	mlx_destroy_image(game->mlx_id, game->img_collectible);
+	mlx_destroy_image(game->mlx_id, game->img_exit);
+	mlx_destroy_image(game->mlx_id, game->img_player_w);
+	mlx_destroy_image(game->mlx_id, game->img_player_a);
+	mlx_destroy_image(game->mlx_id, game->img_player_s);
+	mlx_destroy_image(game->mlx_id, game->img_player_d);
+	free_map(game->map);
 	//ft_printf("map[1] ; %s\n", game->map[1]);
 	mlx_destroy_window(game->mlx_id, game->win_id);
 	exit(0);
