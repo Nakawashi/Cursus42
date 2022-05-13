381de73ae953b87608b26c065931dd44236560b4
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-05-13 14:36:47 +0200

    comment functions

--- so_long/sources/hooks.c
+++ so_long/sources/hooks.c
@@ -6,12 +6,15 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 12:59:31 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/05/09 15:12:00 by lgenevey         ###   ########.fr       */
+/*   Updated: 2022/05/13 11:46:14 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+/*
+	handle when we press ESC
+*/
 static int	key_hook(int keycode, t_game *game)
 {
 	if (keycode == ESC)
@@ -19,6 +22,9 @@ static int	key_hook(int keycode, t_game *game)
 	return (0);
 }
 
+/*
+	handle when we close the window
+*/
 void	game_hooks(t_game *game)
 {
 	mlx_key_hook(game->win_id, key_hook, &game);
