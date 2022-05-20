32cde87dd8c31d929a460619568efe921ebfc1d7
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-05-20 13:35:17 +0200

    first push

---
+++ push_swap/doc/info.md
@@ -0,0 +1,14 @@
+
+
+Instructions must be separated by a ’\n’ and nothing else
+
+If no parameters are specified, the program must not display anything and give
+the prompt back.
+
+In case of error, you must display "Error" followed by a ’\n’ on the standard
+error. Errors include for example: some arguments are not integers, some arguments
+are bigger than an integer, there are duplicates, an instruction doesn’t exist
+and/or is incorrectly formatted.
+
+parser user infos
+faire les fonction des mouvements autorises
---
+++ push_swap/sources/push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 12:15:01 by lgenevey          #+#    #+#             */
+/*   Updated: 2022/05/20 12:16:20 by lgenevey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libraries/libft/libft.h"
+# include "../libraries/libft/ft_printf/ft_printf.h"
+
+#endif
