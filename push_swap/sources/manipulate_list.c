0c10022636de0c344e7c7c91b0d76748d911c147
Lucie Genevey <l.genevey@gmail.com>
2022-05-30 11:41:35 +0200

    reflexion sur les fonctions concernant la liste

--- push_swap/sources/manipulate_list.c
+++ push_swap/sources/manipulate_list.c
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 16:07:36 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/05/28 14:59:05 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/05/29 19:02:17 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 // creates and add new element in stack
 void	add_elem_at_top(t_stack *stack, int number)
 {
-	t_book *new = (t_book *)malloc(sizeof(*new));
-	if (stack == NULL || new == NULL)
-		ft_printf("erreur, quitter");
+
 }
 
