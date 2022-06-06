f4d413b494ac15a4eb7d8ece009a434d81e0c441
Lucie Genevey <l.genevey@gmail.com>
2022-06-06 17:14:58 +0200

    check if is_integer and is_duplicates

--- push_swap/sources/parse_argv.c
+++ push_swap/sources/parse_argv.c
@@ -3,19 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parse_argv.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 15:03:13 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/06/04 17:58:49 by lgenevey         ###   ########.fr       */
+/*   Updated: 2022/06/06 17:10:38 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-// static int	check_duplicates(char **data_to_check)
-// {
-// 	return (0);
-// }
 
 /*
 	1. get user datas and return them in an array of pointers
@@ -24,33 +19,27 @@
 	3. no duplicated numbers
 
 	display errors if not.
-
 	returns **char string
 */
+
+static int	is_integer(char **char)
+{
+
+}
+
 void	**parse_arguments(int argc, char **argv)
 {
 	int	nb_of_args;
 	char **values;
 
 	nb_of_args = argc--; //dont want to count prg name
+	values = argv++;
 	if (nb_of_args == 2)
 		values = ft_split(argv[0], ' ');
 	else
-	{
-		values = argv++; // start after prg name
-		// values = (char **)malloc(sizeof(char *) * nb_of_args + 1);
-		// if (!values)
-		//	return (NULL);
-		if (are_integers(values) && no_duplicates(values))
-			return (values);
-	}
-	//ft_printf("argv : %s\n", argv[0]);
-	//ft_printf("values : %s\n", values[3]);
-	//check_duplicates(values);
-	return (NULL);
+		values = argv; // start after prg name
+	if (!is_integer(values))
+		clean();
+	if (!are_duplicated(values))
+		clean();
 }
-
-/*
-	sinon on stock argv mais a partir du pointeur 1 (0 etant le nom du prg)
-	donc la on a un tableau de pointeurs quil faut checker
-*/
