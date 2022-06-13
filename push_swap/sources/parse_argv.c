693f98f6a744b95fab95ae6809ad7602ae6b042d
Lucie Genevey <l.genevey@gmail.com>
2022-06-13 19:16:33 +0200

    ok

--- push_swap/sources/parse_argv.c
+++ push_swap/sources/parse_argv.c
@@ -6,20 +6,21 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 15:03:13 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/06/13 18:38:26 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/06/13 19:16:04 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	find_duplicate(char **array)
+/*
+	Writes Error at first found duplicated string / number
+*/
+void	find_duplicate(char **array)
 {
 	int		i;
 	int		j;
 	char	*value_to_check;
 
-	if (!array)
-		return (0);
 	i = 0;
 	while (array[i])
 	{
@@ -33,21 +34,18 @@ int	find_duplicate(char **array)
 			j++;
 		}
 	}
-	return (1);
 }
 
 /*
-	parcours chaque caractère du tableau pour definir si ce sont bien des digit
-	tiens compte des nombres negatifs
+	Writes Error if a char is not a digit
+	Does nothing if user writes + or - before the number
 */
-int	is_number(char **array)
+void	is_number(char **array)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	if (!array)
-		return (0);
 	while (array[i])
 	{
 		j = 0;
@@ -61,7 +59,6 @@ int	is_number(char **array)
 		}
 		i++;
 	}
-	return (1);
 }
 
 /*
@@ -71,6 +68,8 @@ char	**get_user_data(int argc, char **argv)
 {
 	char	**values;
 
+	if (!argv)
+		return (NULL);
 	values = ++argv;
 	if (argc == 2)
 		values = ft_split(argv[0], ' ');
