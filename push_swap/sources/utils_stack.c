459a32d1735ec1d9953d69fb1a045c9e4f397fb5
Lucie Genevey <l.genevey@gmail.com>
2022-06-18 17:20:20 +0200

    error in fill_stack

--- push_swap/sources/utils_stack.c
+++ push_swap/sources/utils_stack.c
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 13:27:02 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/06/17 14:21:02 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/06/18 17:19:03 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,20 @@ int	fill_stack(int *num, t_stack *stack)
 {
 	int		i;
 	t_list	*new;
-	printf("num: [%d]\n", num[0]);
-	printf("num: [%d]\n", num[1]);
-	printf("num: [%d]\n", num[2]);
-	printf("num: [%d]\n", num[3]);
+
 	init_stack(stack);
 	stack->size = ft_count_indexes(num);
-	printf("stack->size : %d\n", stack->size);
 	if (stack->size == 1)
 		return (0);
 	i = 0;
-	while(num[i])
+	while(i < stack->size)
 	{
 		new = ft_lstnew(&num[i]);
 		if (!new)
 			return (0);
 		ft_lstadd_back(&(stack->top), new);
-		printf("new->content: %d\n", *((int *)new->content));
+		printf("num[i] : %d\n", num[i]);
+		printf("new->content[i] : %d\n", *((int *)new->content));
 		i++;
 	}
 	return (1);
