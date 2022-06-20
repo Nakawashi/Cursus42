98b1eb977e7a4987b1e3e50128a656381dd4f73e
Lucie Genevey <l.genevey@gmail.com>
2022-06-20 23:53:02 +0200

    pb avec from.top.content

--- push_swap/sources/operations/push.c
+++ push_swap/sources/operations/push.c
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:22:12 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/06/20 18:17:05 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/06/20 23:50:31 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,36 @@
 */
 static void	push(t_stack *from, t_stack *to)
 {
-	if (to->size == 0)
-	to->size++;
-	from->size--;
+	t_list	*actual_to_top;
+
+	if (!from || !to)
+		return ;
+	from->size = ft_lstsize(from->top);
+	to->size = ft_lstsize(to->top);
+	if (to->size) // si deja qqch dans to
+		actual_to_top = to->top;
+	printf("from initial size : %d\n", from->size);
+	printf("to initial size : %d\n", to->size);
+
+	ft_lstadd_front(&(to->top), from->top); // passer de l'un à l'autre
+
+	if (to->size == 0) // si b était vide, le nouvel element est le seul
+		to->top->next = NULL;
+	else
+		to->top->next = actual_to_top;
+	from->size -= 1;
+	to->size += 1;
+	if (from->size > 1)
+		from->top = from->top->next; // changer premier élément de from
+	else if (from->size == 1)
+		from->top->next = NULL;
+
+	printf("numéro passé dans b : %d\n", *((int *)to->top->content));
+	printf("nouveau premier élément de a : %d\n", *((int *)from->top->content));
+	printf("from size : %d\n", from->size);
+	printf("to size : %d\n", to->size);
 }
+
 /*
 	push element from b to a
 */
