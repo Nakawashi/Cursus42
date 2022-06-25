660db7793ece025530a094dcfead5f1700994b83
Lucie Genevey <l.genevey@gmail.com>
2022-06-25 15:13:23 +0200

    sort 5 update - rra instead of ra if last == min

--- push_swap/sources/push_swap.h
+++ push_swap/sources/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 12:15:01 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/06/25 13:55:08 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/06/25 15:11:40 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 # define INT_MIN -2147483648
 # define INT_MAX 2147483647
 
-# define TEST 1
+# define TEST 0
 
 typedef struct s_stack
 {
--- push_swap/sources/small_sort.c
+++ push_swap/sources/small_sort.c
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 16:13:10 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/06/25 14:02:00 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/06/25 14:59:49 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,39 +50,17 @@ void	sort_3_elements(t_stack *a)
 	Separe les deux valeurs plus petites
 	Trier les trois valeurs restantes
 */
-/* void	sort_5_elements(t_stack *a, t_stack *b)
-{
-	int	min;
-	int	last;
-
-	while (a->size > 3)
-	{
-		min = get_min_value(a);
-		last = *((int *)(ft_lstlast(a->top))->content);
-		if (last == min)
-			ra(a);
-		if (*((int *)a->top->content) == min)
-			pb(a, b);
-		else
-			ra(a);
-	}
-	sort_3_elements(a);
-	while (b->size)
-		pa(b, a);
-} */
 void	sort_5_elements(t_stack *a, t_stack *b)
 {
 	int	min;
-	int	max;
 	int	last;
 
-	if ()
 	while (a->size > 3)
 	{
 		min = get_min_value(a);
 		last = *((int *)(ft_lstlast(a->top))->content);
 		if (last == min)
-			ra(a);
+			rra(a);
 		if (*((int *)a->top->content) == min)
 			pb(a, b);
 		else
@@ -92,5 +70,7 @@ void	sort_5_elements(t_stack *a, t_stack *b)
 	while (b->size)
 		pa(b, a);
 }
+
+
 
 // if get min > 12, use fonction get max
