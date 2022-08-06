2121e30174bb079ffb71950e46e4513cb075f0fa
Lucie Genevey <l.genevey@gmail.com>
2022-08-06 14:17:09 +0200

    add mini_libft functions

--- philosopher/includes/philo.h
+++ philosopher/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:06:14 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/08/06 13:53:25 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/08/06 14:15:26 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <unistd.h>
 # include <sys/time.h>
 
+// mini_libft : because libft is not allowed
 
+int		ft_atoi(const char *str);
+int		ft_strlen(char *str);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int n);
 
 #endif
