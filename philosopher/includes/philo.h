ee6204ac29d45b0ed7d0cd81baa7ee4fbb3e38c5
Lucie Genevey <l.genevey@gmail.com>
2022-08-06 14:02:45 +0200

    testing gettimeofday

--- philosopher/includes/philo.h
+++ philosopher/includes/philo.h
@@ -6,13 +6,17 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:06:14 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/08/05 12:07:23 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/08/06 13:53:25 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdio.h>
 # include <unistd.h>
+# include <sys/time.h>
+
+
 
 #endif
