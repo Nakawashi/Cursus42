4e6a5a2247f3713c5677659d5428bc9810248b8b
Lucie Genevey <l.genevey@gmail.com>
2022-09-05 13:16:49 +0200

    update -- setup routine

---
+++ philosopher/sources/routine.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 11:30:59 by nakawashi         #+#    #+#             */
+/*   Updated: 2022/09/05 11:33:48 by nakawashi        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	one_philo_routine(t_global *global)
+{
+	
+	return (0);
+}
+
+int	philos_routine(t_global *global)
+{
+	return (0);
+}
