9f317f556a18396fb88815cb81a61de52ecc1ffa
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-09-06 19:00:33 +0200

    update -- structures

--- philosopher/includes/philo.h
+++ philosopher/includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:06:14 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/09/04 18:25:59 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/09/06 19:00:18 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+typedef pthread_mutex_t	t_mutex;
+typedef pthread_t		t_pthread;
+
 enum e_errors
 {
 	NB_ARGUMENTS = 0,
@@ -27,10 +30,10 @@ enum e_errors
 
 enum e_philo_state
 {
-	THINKING = 0,
-	EATING = 1,
-	SLEEPING = 2,
-	DEAD = 3,
+	THINKING = 1,
+	EATING,
+	SLEEPING,
+	DEAD
 };
 
 typedef struct s_args
@@ -42,31 +45,27 @@ typedef struct s_args
 	int	number_of_times_each_philosopher_must_eat;
 }	t_args;
 
+typedef struct s_rules
+{
+	t_args			*args;
+	t_philo			*philos;
+	t_mutex			*fork;
+	t_mutex			writing;
+	unsigned int	prog_start;
+	unsigned int	timestamp_in_ms;
+}	t_rules;
+
 typedef struct s_philo
 {
 	unsigned		id;
 	int				fork_in_hands;
 	unsigned int	state;
 	unsigned int	meal_eaten;
-
+	unsigned int	last_meal;
+	pthread_mutex_t	fork_left;
+	pthread_mutex_t	fork_right;
+	t_rules			*rules;
 }	t_philo;
-
-typedef struct s_fork
-{
-	int	id;
-	int	state;
-}	t_fork;
-
-typedef struct s_global
-{
-	t_args			*args;
-	t_philo			*philos;
-	t_fork			*forks;
-	unsigned int	prog_start;
-	unsigned int	timestamp_in_ms;
-	pthread_mutex_t	mutex1;
-	pthread_mutex_t	mutex2;
-}	t_global;
 
 // mini_libft.c : because entire libft is not allowed
 
@@ -78,13 +77,12 @@ void	ft_putnbr(int n);
 
 // init.c : init structures' variables
 void	init_args(int argc, char **argv, t_args *args);
-t_philo	*create_philos(t_args *args, t_global *global);
-t_fork	*create_forks(t_args *args);
+t_philo	*create_philos(t_args *args, t_rules *global);
 
 // utils.c : fonctions utiles propre à ce projet
 int		error(int type_of_error);
 int		is_even(int n);
 int		are_args_valid(int argc, char **argv);
-void	get_time_in_ms(struct timeval *tp, t_global *global);
+void	get_time_in_ms(struct timeval *tp, t_rules *global);
 
 #endif
