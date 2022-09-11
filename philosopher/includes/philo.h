503c7bf4883f50ab455898f2261d6afb330a1bea
Lucie Genevey <l.genevey@gmail.com>
2022-09-11 16:08:01 +0200

    some updates

--- philosopher/includes/philo.h
+++ philosopher/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:06:14 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/09/11 14:17:18 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/09/11 14:40:13 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,16 +45,6 @@ typedef struct s_args
 	int	number_of_times_each_philosopher_must_eat;
 }	t_args;
 
-typedef struct s_rules
-{
-	t_args			*args;
-	t_philo			*philos;
-	t_mutex			*fork;
-	t_mutex			writing;
-	unsigned int	prog_start;
-	unsigned int	timestamp_in_ms;
-}	t_rules;
-
 typedef struct s_philo
 {
 	unsigned		id;
@@ -64,22 +54,17 @@ typedef struct s_philo
 	pthread_t		thread;
 }	t_philo;
 
-typedef struct s_fork
-{
-	int	id;
-	int	state;
-}	t_fork;
-
-typedef struct s_global
+typedef struct s_rules
 {
 	t_args			*args;
-	t_philo			*philos;
-	t_fork			*forks;
+	t_philo			*philos_array;
 	unsigned int	prog_start;
 	unsigned int	timestamp_in_ms;
 	pthread_mutex_t	fork;
 	pthread_mutex_t	printed_text;
-}	t_global;
+}	t_rules;
+
+
 
 // mini_libft.c : because entire libft is not allowed
 
@@ -91,13 +76,13 @@ void	ft_putnbr(int n);
 
 // init.c : init structures' variables
 void	init_args(int argc, char **argv, t_args *args);
-t_philo	*create_philos(t_args *args, t_rules *global);
+t_philo	*create_philos(t_args *args, t_rules *rules);
 
 // utils.c : fonctions utiles propre à ce projet
 int		error(int type_of_error);
 int		is_even(int n);
 int		are_args_valid(int argc, char **argv);
-void	get_time_in_ms(struct timeval *tp, t_global *global);
+void	get_time_in_ms(struct timeval *tp, t_rules *rules);
 void	routine(void);
 
 #endif
