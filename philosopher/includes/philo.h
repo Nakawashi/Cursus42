d08c6f2c3a8b82971a74afd1e35a767de0f54aa7
Lucie Genevey <l.genevey@gmail.com>
2022-09-13 01:26:30 +0200

    update

--- philosopher/includes/philo.h
+++ philosopher/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:06:14 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/09/13 00:00:06 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/09/13 01:20:27 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ enum e_errors
 	ERR_THREAD_CREATION,
 	ERR_MUTEX_INIT,
 	ERR_MALLOC,
+	ERR_JOIN_THREAD,
+	ERR_MUTEX_DESTROY,
 };
 
 enum e_philo_state
@@ -44,7 +46,7 @@ typedef struct s_args
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
-	int	number_of_times_each_philosopher_must_eat; // nb eat
+	int	nb_of_times_each_philo_must_eat; // nb eat
 }	t_args;
 
 typedef struct s_rules
@@ -60,12 +62,13 @@ typedef struct s_rules
 typedef struct s_philo
 {
 	unsigned int	id;
-	unsigned int	eat_count;
+	int				eat_count;
 	long long		last_meal;
 	pthread_t		thread;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
-	t_rules			*rules;
+	t_rules			*rules;		//question, pour pouvoir utiliser dans routine ?
+	t_args			*args;
 }	t_philo;
 
 // mini_libft.c : because entire libft is not allowed
@@ -79,12 +82,16 @@ void	ft_putnbr(int n);
 void	init_args(int argc, char **argv, t_args *args);
 int		init_rules(t_rules *rules, t_args *args);
 
-
 // utils.c : fonctions utiles propre à ce projet
 int			error(int type_of_error);
 int			is_even(int n);
 int			are_args_valid(int argc, char **argv);
 long long	get_time_in_ms(void);
-void		*routine(void *philo);
+
+// start_simulation.c : le démarrage du tout
+int	start_simulation(t_rules *rules, t_args *args);
+
+// routine.c : la routine, vie de philosophes
+void	*routine(void *philo_array);
 
 #endif
