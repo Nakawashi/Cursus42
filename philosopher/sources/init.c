042cb36af0f0e806f5ececf3b6168c70057e685f
Lucie Genevey <l.genevey@gmail.com>
2022-09-18 15:22:21 +0200

    nb time they must eat = 0, not -1

--- philosopher/sources/init.c
+++ philosopher/sources/init.c
@@ -6,7 +6,7 @@
 /*   By: nakawashi <nakawashi@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 14:27:23 by nakawashi         #+#    #+#             */
-/*   Updated: 2022/09/17 14:21:42 by nakawashi        ###   ########.fr       */
+/*   Updated: 2022/09/18 15:21:03 by nakawashi        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	init_args(int argc, char **argv, t_rules *rules)
 	rules->args.time_to_die = (long long)ft_atoi(argv[2]);
 	rules->args.time_to_eat = (long long)ft_atoi(argv[3]);
 	rules->args.time_to_sleep = (long long) ft_atoi(argv[4]);
-	rules->args.nb_of_times_they_must_eat = -1;
+	rules->args.nb_of_times_they_must_eat = 0;
 	if (argc == 6)
 		rules->args.nb_of_times_they_must_eat = ft_atoi(argv[5]);
 /* 	printf("\nCheck init args, philos and rules structures : \n");
@@ -36,19 +36,23 @@ void	init_args(int argc, char **argv, t_rules *rules)
 
 /*
 	Initialise chaque variable du tableau de structures philos_array
-	Initialise les mutex pour chaque fourchette
+	Initialise les mutex pour chaque fourchette : on met pas toutes
+	les fourchettes dans un seul mutex pour éviter le deadlock (ils
+	commencent tous à prendre une fourchettent en même temps et attendent
+	un temps infini pour prendre la seconde)
+	Mettre l'adresse d'une fourchette du tableau dans la "main gauche" du pholo
+	Mettre l'adresse i + 1 % nb_philos dans la main droite
+	Initialiser un mutex pour la fourchette i car "la sienne"
 	Retourne 0 si tout s'est bien passé
 */
 static int	init_philos(t_rules *rules)
 {
 	int	i;
-	int	id;
 
-	i = 0;
-	id = 1;
-	while (i <= rules->args.nb_philos)
+	i = -1;
+	while (++i <= rules->args.nb_philos)
 	{
-		rules->philos_array[i].id = id;
+		rules->philos_array[i].id = i + 1;
 		rules->philos_array[i].eat_count = 0;
 		rules->philos_array[i].last_meal = get_time_in_ms();
 		rules->philos_array[i].left_fork = &rules->fork_array[i];
@@ -56,12 +60,13 @@ static int	init_philos(t_rules *rules)
 		rules->philos_array[i].rules = rules;
 		if (pthread_mutex_init(&rules->fork_array[i], NULL))
 			return (error(ERR_MUTEX_INIT));
-		++i;
-		++id;
 	}
 	return (0);
 }
 
+/*
+	Malloc here two pointers, philos and fork arrays
+*/
 int	init_rules(t_rules *rules)
 {
 	rules->philos_array = NULL;
